6ecfab0e90728724a83efd3109950e1126fa10ce
Mike Wells <khsdrummer2001@gmail.com>
2013-05-21 12:38:59 -0500

    stuff

---
+++ Assets/Resources/Scripts/Enemies/Drifter.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class Drifter : Enemy {
+
+    protected override void Update() {
+        base.Update();
+    }
+
+    protected override void OnBecameInvisible() {
+        base.OnBecameInvisible();
+    }
+}
